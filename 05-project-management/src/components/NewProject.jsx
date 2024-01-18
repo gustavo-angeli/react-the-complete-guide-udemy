844ac4e65e8c5cfaff625420ebe62452fe3ca4e2
gustavo-angeli <dev.gustavosilva@outlook.com>
2024-01-18 14:33:32 -0300

    feature: Validating User Input & Showing an Error Modal via useImperativeHandle

--- 05-project-management/src/components/NewProject.jsx
+++ 05-project-management/src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import Input from "./Input"
 import { useRef } from "react"
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd, onCancel }) {
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -17,28 +20,37 @@ export default function NewProject({ onAdd, onCancel }) {
                 description: currentDescription,
                 dueDate: currentDueDate
             });
+            return;
         }
+        modal.current.open();
     }
     
-    return <div className="w-[35rem] mt-16">
-        <menu className="flex items-center justify-end gap-4 my-4">
-            <li>
-                <button 
-                    className="text-stone-800 hover:text-stone-950 bg-stone-200 hover:bg-stone-300 px-6 py-2 rounded-md"
-                    onClick={() => onCancel()}
-                >Cancel</button>
-            </li>
-            <li>
-                <button 
-                    className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-                    onClick={handleSave}
-                >Save</button>
-            </li>
-        </menu>
-        <div>
-            <Input type="text" ref={title} label='Title'/>
-            <Input ref={description} label='Description' isTextArea/>
-            <Input type="date" ref={dueDate} label='Due Date'/>
+    return <>
+        <Modal ref={modal} buttonCaption="Okay" >
+            <h2>Invalid Input</h2>
+            <p>Oops... looks like you forgot to enter a value.</p>
+            <p>Please make sure you provide a value value for every input field</p>
+        </Modal>
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li>
+                    <button 
+                        className="text-stone-800 hover:text-stone-950 bg-stone-200 hover:bg-stone-300 px-6 py-2 rounded-md"
+                        onClick={() => onCancel()}
+                    >Cancel</button>
+                </li>
+                <li>
+                    <button 
+                        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                        onClick={handleSave}
+                    >Save</button>
+                </li>
+            </menu>
+            <div>
+                <Input type="text" ref={title} label='Title'/>
+                <Input ref={description} label='Description' isTextArea/>
+                <Input type="date" ref={dueDate} label='Due Date'/>
+            </div>
         </div>
-    </div>
+    </>
 }
--- 05-project-management/src/components/SideBar.jsx
+++ 05-project-management/src/components/SideBar.jsx
@@ -5,7 +5,7 @@ export default function SideBar({ onStartAddProject, projects }) {
         <aside className="w-1/3 px-8 py-16 bg-slate-900 text-stone-50 md:w-72 rounded-r-xl">
             <h2 className="mb-8  font-bold uppercase md:text-xl text-stone-50">Your Projects</h2>
             <Button onClick={onStartAddProject}>+ Add project</Button>
-            {<ul>
+            {<ul className='mt-8'>
                 {projects.map(project => 
                     <li key={project.id}>
                         <button className='w-full text-left px-2 py-1 rounded my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800'>{project.title}</button>
